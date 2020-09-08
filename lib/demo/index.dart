6e9cf6692a70525eab76176c37bcfb72e0d9c565
王晋杰 <2310385247@qq.com>
2020-09-08 17:15:31 +0800

    项目初始化

---
+++ lib/demo/index.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'input.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: InputDemo());
+  }
+}
---
+++ lib/main copy.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import './pages/index.dart';
+// import './demo/index.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+          title: '百姓生活+',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(primaryColor: Colors.pink),
+          home: IndexPage()),
+    );
+  }
+}
